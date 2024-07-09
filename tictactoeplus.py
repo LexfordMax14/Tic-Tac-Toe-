cf8bf3ee847c2152afbfb12218636c4255375028
LexfordMax14 <lazaro.narvaezu14@gmail.com>
2024-07-09 12:10:34 -0400

    Update tictactoeplus.py
    El ultimo :)

--- tictactoeplus.py
+++ tictactoeplus.py
@@ -27,14 +27,14 @@ def is_cell_occupied(cell, lower_color, upper_color):
     show(cell)
     mask = cv2.inRange(cell, lower_color, upper_color)
     show(mask)
-    return cv2.countNonZero(mask) > 0
+    return cv2.countNonZero(mask) > 10
 
 
 # Función para capturar y procesar el estado del tablero
 def estatus_tablero():
     try:
         robot.set_joints(0, 90, 90)
-
+        time.sleep(2)
         # Captura la imagen del tablero del robot
         frame = robot.get_frame()
         cv2.imwrite("frame.jpg", frame)
@@ -54,7 +54,7 @@ def estatus_tablero():
         # Toma una porción de la imagen utilizando ancho y alto
         h, w = image.shape
         image = image[0:int(h * 0.7), int(w * 0.25):int(w * 0.7)]
-
+        frame_cell= frame[0:int(h * 0.7), int(w * 0.25):int(w * 0.7)]
         # Erosiona la imagen
         kernel = np.ones((5, 5), np.uint8)
         erosion = cv2.erode(image, kernel, iterations=1)
@@ -82,46 +82,116 @@ def estatus_tablero():
                 cv2.line(line_only_image, (x1, y1), (x2, y2), (255, 255, 255), 5)
                 print(f"Line: ({x1}, {y1}) -> ({x2, y2})")
 
+
+        show(line_only_image)
         # Encuentra contornos
         contours, hierarchy = cv2.findContours(cv2.cvtColor(line_only_image, cv2.COLOR_BGR2GRAY), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+        
+        cells=[None]*9
+        print(cells)
         for cnt in contours:
-            cells=[]
+            
             x, y, w, h = cv2.boundingRect(cnt)
 
-            if cnt <1000:
-                cell=cv2.rectangle(line_only_image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                cell = image[y:y+h, x:x+w]
-                cells.append(cell)
+            if cv2.contourArea(cnt) <5500 and cv2.contourArea(cnt)>1000 :
+                print(x, y, w, h)
+                image=cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                cell = frame_cell[y:y+h, x:x+w]
+                cx= x+(w/2)+110
+                cy= y+((h)/2)
+                print(cx)
+                print(cy)
+                if (cx>=150 and cx<=220):
+
+                    if (cy>=50 and cy<=120):
+                        cells[0]=cell
+                        print("lo ingrese a la pos 0")
+
+                    elif (cy>=120 and cy<=190):
+                        cells[3]=cell
+                        print("lo ingrese a la pos 3")
+
+                    elif (cy>=190 and cy<=270):
+                        cells[6]=cell  
+                        print("lo ingrese a la pos 6")
+
+                    else:
+                        print("No identifico celda") 
+
+                elif (cx>=220 and cx<=290):
+
+                    if (cy>=50 and cy<=120):
+                        cells[1]=cell
+                        print("lo ingrese a la pos 1")
+
+                    elif (cy>=120 and cy<=190):
+                        cells[4]=cell
+                        print("lo ingrese a la pos 4")
+
+                    elif (cy>=190 and cy<=270):
+                        cells[7]=cell 
+                        print("lo ingrese a la pos 7")
+
+                    else:
+                        print("No identifico celda")
+
+                elif (cx>=290 and cx<=360):
+
+                    if (cy>=50 and cy<=120):
+                        cells[2]=cell
+                        print("lo ingrese a la pos 2")
+
+                    elif (cy>=120 and cy<=190):
+                        cells[5]=cell
+                        print("lo ingrese a la pos 5")
+
+                    elif (cy>=190 and cy<=270):
+                        cells[8]=cell  
+                        print("lo ingrese a la pos 8")
+                    else:
+                        print("No identifico celda")
+                
+                else:
+                    print("No identifico celda")
+        
+                print(cell, "aqui va la celda")
+                show(cell)
             
+        show(frame_cell)
     
+        print(f"Número de celdas detectadas: {len(cells)}")
+
         # Define los límites de color para detectar las marcas
-        lower_color_x = np.array([56, 31, 0])  # Ejemplo de color para "X"
-        upper_color_x = np.array([122, 141, 83])
-        lower_color_o = np.array([0, 0, 111])  # Ejemplo de color para "O"
-        upper_color_o = np.array([61, 128, 145])
+        lower_color_x = np.array([116, 31, 0])  # Ejemplo de color para "X"
+        upper_color_x = np.array([179, 176, 102])
+        lower_color_o = np.array([95, 60, 0])  # Ejemplo de color para "O"
+        upper_color_o = np.array([155, 255, 255])
 
-        # Procesa cada celda para determinar si está ocupada
 
-        is_cell_occupied
+        if len(cells) != 9: 
+            print(f"Error: Se detectaron {len(cells)} celdas en lugar de 9.")
+            return None
+        
+        print(cells)
+
+
+
         board_status = []
         for idx, cell in enumerate(cells):
-            
+
             hsvImage = cv2.cvtColor(cell, cv2.COLOR_BGR2HSV)
 
             occupied_x = is_cell_occupied(hsvImage, lower_color_x, upper_color_x)
             occupied_o = is_cell_occupied(hsvImage, lower_color_o, upper_color_o)
             if occupied_x:
                 board_status.append(1)  # X
+                print("añadi un 1")
             elif occupied_o:
+                print("añadi un 0")
                 board_status.append(0)  # O
             else:
                 board_status.append(None)
-
-        # Muestra el estado del tablero
-        for i in range(3):
-            print(board_status[i * 3:(i + 1) * 3])
-
+            print(board_status)
         return board_status
 
     except Exception as e:
@@ -160,7 +230,7 @@ def cambio_coord_mov(cx, cy, cz):
 
 def dibujar_x(x, y, z):
 
-    half_size = 70 // 2
+    half_size = 70 // 3
     pt1 = (x - half_size, y - half_size, z)
     pt2 = (x + half_size, y + half_size, z)
     pt3 = (x - half_size, y + half_size, z)
@@ -321,6 +391,7 @@ def jugar_gato():
             # Crea el árbol MiniMax y decide el mejor movimiento
             tree = MiniMaxTree(Node(board_status))
             best_move = tree.best_action
+            print(tree.best_action)
 
             # Mueve el robot según el mejor movimiento
             if best_move == 0:
@@ -342,17 +413,18 @@ def jugar_gato():
             elif best_move == 8:
                 marca_casilla_9()
 
+            # Espera la interacción del jugador y vuelve a escanear el tablero
+            input("Presiona Enter después de que el jugador haya realizado su movimiento...")
+            board_status = estatus_tablero()
             # Pausa para permitir la visualización y ajuste
-            time.sleep(5)
+            time.sleep(2)
 
             # Captura y procesamiento del estado actual del tablero después del movimiento del robot
-            board_status = estatus_tablero()
+  
             if terminal(board_status):
                 break
 
             # Espera la interacción del jugador y vuelve a escanear el tablero
-            input("Presiona Enter después de que el jugador haya realizado su movimiento...")
-            board_status = estatus_tablero()
 
     except Exception as e:
         print(f"Error en el juego: {e}")
@@ -362,5 +434,7 @@ def jugar_gato():
 
 
 # Función principal para iniciar el juego
+
 if __name__ == "__main__":
     jugar_gato()
+
